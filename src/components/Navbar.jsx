15aa6865e927d7972c293e1f86a8ac406f220f60
Naveen <banjaranaveen89@gmail.com>
2025-12-10 15:54:45 +0530

    Update NavBar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
 import React, { useContext, useState } from 'react'
 import {assets} from '../assets/assets'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext'
 const Navbar = () => {
 
     const [visible, setVisible] = useState(false)
     const {setShowSearch,getCartCount}=useContext(ShopContext)
+
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const handleSearchClick = () => {
+        if (location.pathname !== "/collection") {
+            navigate("/collection");  // redirect
+        }
+        setShowSearch(true);  // open search bar
+    };
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
@@ -31,7 +41,7 @@ const Navbar = () => {
             </NavLink>
         </ul>
         <div className='flex items-center gap-6'>
-            <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt=''/>
+            <img onClick={handleSearchClick} src={assets.search_icon} className='w-5 cursor-pointer' alt=''/>
 
             <div className='group relative '>
                 <Link to={'/login'}><img className='w-5 cursor-pointer' src={assets.profile_icon} alt=''/></Link>
