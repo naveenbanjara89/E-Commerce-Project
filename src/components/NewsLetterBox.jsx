1fb5fb89684131bf6adb1f07d75f1f12ac1abc1c
Naveen <banjaranaveen89@gmail.com>
2025-12-10 15:31:13 +0530

    commit NewsLetterBox.jsx file again

--- src/components/NewsLetterBox.jsx
+++ src/components/NewsLetterBox.jsx
@@ -9,9 +9,9 @@ const NewsLetterBox = () => {
   return (
     <div>
         <div>
-            <div className='text center px-70'>
-                <p className='text-xl sm:text-2xl text-gray-800 font-medium px-4 sm:px-25'>Subscribe now & get 20% off</p>
-                <p className='text-gray-500 mt-2 sm:mt-3 text-sm sm:text-base px-4 sm:px-0'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <div className='text-center px-4 xs:px-6 sm:px-70'>
+                <p className='text-lg xs:text-xl sm:text-2xl text-gray-800 font-medium px-3 xs:px-4 sm:px-25'>Subscribe now & get 20% off</p>
+                <p className='text-gray-500 mt-1 xs:mt-2 sm:mt-3 text-xs xs:text-sm sm:text-base px-3 xs:px-4 sm:px-0'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
         </div>
         <div>
